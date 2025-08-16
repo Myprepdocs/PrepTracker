3f5caaf21fe9b04858b5a69be3311f5241ab8a6b
myprepdocs <alan@reeveszone.com>
2025-08-16 18:55:46 +0100

    New diary facility pass 1

--- js/version-sw.js
+++ js/version-sw.js
@@ -1,4 +1,4 @@
 // Service Worker version constant
 // This file provides the version constant to the service worker
 // It must be kept in sync with js/version.js
-self.APP_VERSION = '1.0.30';
+self.APP_VERSION = '1.0.31';
